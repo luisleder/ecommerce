9fd0041d3f01cf78804fee705fc51f907000b3d7
Luis Leder <leaderluis20@gmail.com>
2023-12-15 21:21:40 -0300

    add definition of router product crud

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\ProducController;
+use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\UserController;
 
 /*
@@ -18,3 +17,16 @@ Route::controller(UserController::class)->group(function(){
     
 });
 
+Route::controller(ProductController::class)->group(function(){
+
+    Route::post('product','store');
+    Route::put('product','update');
+    Route::get('product','index');
+    Route::get('product/{id}','show');
+    Route::delete('product','destroy');
+
+})->middleware("auth:api");
+
+Route::fallback(function(){
+    return response()->json(['errors' => "Invalid route"], 404);
+});
