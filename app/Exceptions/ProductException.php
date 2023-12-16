de0c865793266e99e804e7ea1faa31372d0c3f94
Luis Leder <leaderluis20@gmail.com>
2023-12-15 22:29:06 -0300

    fix custom product exception

---
+++ app/Exceptions/ProductException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Exceptions;
+
+class ProductException extends CustomException
+{
+    public static function notExist()
+    {
+        return new self(message: "Product not found", code: 404);
+    }
+    
+}
